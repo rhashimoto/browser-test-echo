0727b3bd59178d2ab569e5b25f086fdde875ad7e
Roy Hashimoto <roy@shoestringresearch.com>
2021-01-23 16:51:40 -0800

    Rename fallback index file.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Jest doesn't honor the exports field in package.json, so this
-// index.mjs file is fortunately found by default.
+// index.js file is fortunately found by default.
 import { BrowserTestRunner } from './src/BrowserTestRunner.js';
 export { BrowserTestRunner };
